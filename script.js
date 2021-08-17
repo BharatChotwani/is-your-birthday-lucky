81b34cbf05b77ab95c1fa101819e6a1b001530cd
Bharat <bchotwani3@gmail.com>
2021-08-17 19:51:40 +0530

    Update JS

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumber = document.querySelector("#check-number");
 
-checkNumber.addEventListener('click',function getValue(){
-    console.log(dateOfBirth.value, luckyNumber.value);
-}) 
+function compareValues(sum, luckyNumber){
+    if(sum%luckyNumber===0){
+     console.log("Your birthday is lucky");
+    }
+    else {
+        console.log("Your birthday isn't lucky");
+    }
+}
+
+function checkBirthdayDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    compareValues(sum, luckyNumber.value)
+}
+
+function calculateSum(dob){
+     dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let index=0; index<dob.length; index++){
+        sun = sum + Number(dob.charAt(index));
+    }
+
+    return sum;
+}
+
+checkNumber.addEventListener('click',checkBirthdayDateIsLucky)
